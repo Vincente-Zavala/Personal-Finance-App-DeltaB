c1e459315e13c188ae694c1070a4aa0588f32a66
Vincent Zavala <vincentzavala73@gmail.com>
2025-10-20 18:40:10 -0500

    Delta B

--- Delta/asgi.py
+++ Delta/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Budget project.
+ASGI config for Delta project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Budget.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Delta.settings")
 
 application = get_asgi_application()
