7cc53e82d875e89965db63a60be434f296865673
Vincent Zavala <vincentzavala73@gmail.com>
2025-11-10 18:12:05 -0600

    db update

--- DeltaB/settings.py
+++ DeltaB/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = "DeltaB.wsgi.application"
 DATABASES = {
     "default": dj_database_url.parse(
         "REDACTED_DB_URL/postgres",
-        conn_max_age=30,
+        conn_max_age=60,
         ssl_require=True
     )
 }
