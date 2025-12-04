c6c76699c3fd7a6a31b4e94334bb2ec0a393e98a
Vincent Zavala <vincentzavala73@gmail.com>
2025-12-03 20:46:13 -0600

    reqtxt

--- DeltaB/settings.py
+++ DeltaB/settings.py
@@ -13,11 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 
 
 # Quick-start development settings - unsuitable for production
